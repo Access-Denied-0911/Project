1a3e4213bdb6a3dda1cf1ec2e41d3e14292dbebb
Arkoparno De Sarkar <arkaparna.ds@gmail.com>
2026-04-26 15:28:03 +0530

    Update product.js

--- product.js
+++ product.js
@@ -6,14 +6,16 @@ function renderProducts() {
     items.forEach(item => {
         html += `
         <div class="item_container">
-            <img class="sale_image" src="${item.image}">
+            <div class="product-clickable-area" onclick="window.location.href='product-details.html?id=${item.id}'" style="cursor: pointer;">
+                <img class="sale_image" src="${item.image}">
 
-            <div class="rating">
-                ${item.rating.stars} ⭐ | ${item.rating.count}
+                <div class="rating">
+                    ${item.rating.stars} ⭐ | ${item.rating.count}
+                </div>
+
+                <div class="company_name">${item.company}</div>
+                <div class="item_name">${item.item_name}</div>
             </div>
-
-            <div class="company_name">${item.company}</div>
-            <div class="item_name">${item.item_name}</div>
 
             <div class="price">
                 <span class="current_price">₹${item.current_price}</span>
